7fc0f6c98e464ca252d8c6b317579a8c6a38ddc5
Marika-G <33400770+Marika-G@users.noreply.github.com>
2017-12-09 17:51:20 -0500

    Add files via upload

---
+++ 5a.py
@@ -0,0 +1,16 @@
+pas = 0
+
+with open('input.txt', 'r') as chemin:
+	instructions = [int(ligne) for ligne in chemin]
+
+position = 0
+mouvement = instructions[position]
+while(True):
+	try:
+		instructions[position] += 1
+		position += instructions[position] - 1
+		pas += 1
+
+	except IndexError:
+		print(pas)
+		break
