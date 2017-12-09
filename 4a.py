9c0b2b343ab8e6cd84bb9233067eb5859881ba21
Marika-G <33400770+Marika-G@users.noreply.github.com>
2017-12-09 16:14:50 -0500

    Add files via upload

---
+++ 4a.py
@@ -0,0 +1,14 @@
+
+tot = 0
+
+with open('input.txt', 'r') as input:
+	for line in input:
+		tot += 1
+		mySet = set()
+		for x in line.split():
+			if (x not in mySet):
+				mySet.add(x)
+			else:
+				tot -= 1
+				break
+print(tot)
